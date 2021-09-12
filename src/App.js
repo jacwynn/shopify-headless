9efccba9dfbd5b7ce9c314d0d7c55f1e939da52b
jacwynn <jac@wynndigitalweb.com>
2021-09-11 23:00:51 -0400

    add home and product detail pages; setup shop context; install chakra ui and icons; create navbar;

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Cart from "./components/Cart";
+import NavBar from "./components/NavBar";
+
+import Home from "./pages/Home";
+import ProductPage from "./pages/ProductPage";
 
 function App() {
-	return <div className="App">React Shopify Headless</div>;
+	return (
+		<div className="App">
+			<Router>
+				<NavBar />
+				<Cart />
+				<Switch>
+					<Route path="/products/:handle">
+						<ProductPage />
+					</Route>
+					<Route path="/">
+						<Home />
+					</Route>
+				</Switch>
+				<p>Footer</p>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
